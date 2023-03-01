f9fcc3fa5b8fb4833cc76afee95bd897d29bc20e
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-01 12:19:27 +0300

    feat: set error handlers

--- vendor/amp/ErrorInterceptor.php
+++ vendor/amp/ErrorInterceptor.php
@@ -13,6 +13,44 @@ class ErrorInterceptor
         } else {
             error_reporting(NIL);
         }
+
+        // подключаем функции-перехватчики событий
+        set_exception_handler([$this, 'exceptionHandler']);
+        set_error_handler([$this, 'errorHandler']);
+
+        // критичные ошибки отдельно
+        ob_start();
+        register_shutdown_function([$this, 'fatalErrorHandler']);
+
     }
+
+
+    public function errorHandler($errno, $errstr, $errfile, $errline)
+    {
+
+    }
+
+    public function exceptionHandler(\Throwable $e)
+    {
+
+    }
+
+    public function fatalErrorHandler()
+    {
+        $error = error_get_last();
+
+        if (!empty($error && $error['TYPE'] & (E_ERROR | E_PARSE | E_COMPILE_ERROR | E_CORE_ERROR) )) {
+
+            // log error
+            ob_end_clean();
+
+            // display error
+        } else {
+            ob_end_flush();
+        }
+
+    }
+
+
 
 }
